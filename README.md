6f84797f54673fc524801893d31c623793137b4d
ByeolNabi <narwhal_blast@naver.com>
2025-04-17 23:57:49 +0900

    폰트 분석을 위한 python 프로그램 작성
    원하는 폰트의 ascii범위의 문자를 보여줌

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 개발 환경
 mac, vscode, cmake, c++, openCV
+
+## 프로그램 실행 방법
+`이미지 위치는 ./build에 넣어주셔야 합니다.`
+
+```bash
+mkdir build  
+cd build  
+cmake ..  
+make
+./program_name # 파일 실행
+```
--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
+using namespace std;
 
 int main() {
-    Mat img = imread("roro.jpg");
-    if(img.empty()) {
-        printf("이미지 파일을 불러올 수 없습니다.\n");
-        return -1;
-    }
-    imshow("image", img);
-    waitKey(0);
-    return 0;
+  Mat img = imread("roro.jpg");
+  if (img.empty()) {
+    printf("이미지 파일을 불러올 수 없습니다.\n");
+    return -1;
+  }
+  Mat img_gray;
+  cvtColor(img, img_gray, COLOR_BGR2GRAY);
+
+  // 사이즈 변경
+  resize(img, img, Size(img.cols, img.rows));
+  resize(img_gray, img_gray, Size(img_gray.cols >> 1, img_gray.rows >> 1));
+
+  putText(img,                    // 글자를 쓸 이미지
+          "Hello, OpenCV!",       // 출력할 문자열
+          Point(50, 100),         // 시작 좌표(왼쪽 아래 기준)
+          FONT_HERSHEY_COMPLEX,   // 폰트 종류
+          1.0,                    // 폰트 크기 (배율)
+          Scalar(255, 255, 255),  // 색상 (B, G, R) - 흰색
+          2,                      // 두께
+          LINE_AA                 // 선 타입 (안티앨리어싱)
+  );
+
+  imshow("image original", img);
+  imshow("image gray", img_gray);
+  moveWindow("image gray", img.cols, 0);
+
+  waitKey(0);
+  return 0;
 }
